28be49324a789b98c3db7f09f46135c5555958a4
Zaczoozergoodboyrylan <zacwil60@kingshigh.school.nz>
2026-05-18 11:00:26 +1200

    Brendantuff Version 2.1
    Started making AI car

---
+++ brendantuff-test007/Game/ai_car.gd
@@ -0,0 +1,17 @@
+extends PathFollow2D
+@export var max_speed :float = 200.0
+@export var laps :int = 0 
+@export var acceleration :float = 0.5
+
+var speed : float
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	speed += acceleration * delta
+	progress+= speed 
+	print(speed)
